877569331d08e5967b9347a60d095e78d88b7022
Shahidul-PHP <Shahidul.shovon14@gmail.com>
2022-10-11 11:46:43 +0600

    done

--- HomeTaskTwo.php
+++ HomeTaskTwo.php
@@ -1,13 +1,33 @@
 <?php
 //Home Task - 02
 /*  
-    02. Check Letter Voiel or Consonent
+    02. Check Letter Voiel or Consonent using Switch Case
 */
 
+echo '<title>Home Task 02</title>';
+
+$letters = 'i';
 
 
+switch($letters){
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+        echo "The letter is a Vowel";
+    break;
 
+    default:
+        echo "The letter is a Consonant";
 
+}
 
 
 
