9520764c7424bcd3aa42f2392f7c24abc3a85711
Shahidul-PHP <Shahidul.shovon14@gmail.com>
2022-10-11 12:02:29 +0600

    all are done

---
+++ NestedElse.php
@@ -0,0 +1,32 @@
+<?php 
+
+$plOne = 18;
+$plTwo = 'female';
+
+if($plOne >= 18 && $plOne <= 38){
+    if($plTwo == 'male'){
+        echo "you can play";
+    }else{
+        echo "you can not play because you are female";
+    }
+}else{
+    echo "you cannot";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
